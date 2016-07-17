8aaef086d7ea4deb28d018ee38524abe61a146f5
Ying Wang <yingwang@Yings-MacBook-Air-2.local>
2016-07-17 23:23:57 +0200

    first commit

---
+++ ChineseChess/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChineseChess
+//
+//  Created by Ying Wang on 17/07/16.
+//  Copyright © 2016 Ying. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
